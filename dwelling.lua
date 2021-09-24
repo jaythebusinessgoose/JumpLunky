7a8d0a5d9f1ebf09f18d3c4b71f604971e9b1db4
jaythebusinessgoose <jaythebusinessgoose@gmail.com>
2021-09-23 19:45:35 -0600

    What

--- dwelling.lua
+++ dwelling.lua
@@ -100,46 +100,6 @@ dwelling.load_level = function()
         end
     end, ON.FRAME)
 
-
-    -- local removed_embedded_items = {
-    --     ENT_TYPE.ITEM_ALIVE_EMBEDDED_ON_ICE,
-    --     ENT_TYPE.ITEM_PICKUP_ROPEPILE,
-    --     ENT_TYPE.ITEM_PICKUP_BOMBBAG,
-    --     ENT_TYPE.ITEM_PICKUP_BOMBBOX,
-    --     ENT_TYPE.ITEM_PICKUP_SPECTACLES,
-    --     ENT_TYPE.ITEM_PICKUP_CLIMBINGGLOVES,
-    --     ENT_TYPE.ITEM_PICKUP_PITCHERSMITT,
-    --     ENT_TYPE.ITEM_PICKUP_SPRINGSHOES,
-    --     ENT_TYPE.ITEM_PICKUP_SPIKESHOES,
-    --     ENT_TYPE.ITEM_PICKUP_PASTE,
-    --     ENT_TYPE.ITEM_PICKUP_COMPASS,
-    --     ENT_TYPE.ITEM_PICKUP_PARACHUTE,
-    --     ENT_TYPE.ITEM_CAPE,
-    --     ENT_TYPE.ITEM_JETPACK,
-    --     ENT_TYPE.ITEM_TELEPORTER_BACKPACK,
-    --     ENT_TYPE.ITEM_HOVERPACK,
-    --     ENT_TYPE.ITEM_POWERPACK,
-    --     ENT_TYPE.ITEM_WEBGUN,
-    --     ENT_TYPE.ITEM_SHOTGUN,
-    --     ENT_TYPE.ITEM_FREEZERAY,
-    --     ENT_TYPE.ITEM_CROSSBOW,
-    --     ENT_TYPE.ITEM_CAMERA,
-    --     ENT_TYPE.ITEM_TELEPORTER,
-    --     ENT_TYPE.ITEM_MATTOCK,
-    --     ENT_TYPE.ITEM_BOOMERANG,
-    --     ENT_TYPE.ITEM_MACHETE,
-    --     ENT_TYPE.EMBED_GOLD,
-    --     ENT_TYPE.EMBED_GOLD_BIG,
-    --     ENT_TYPE.ITEM_RUBY,
-    --     ENT_TYPE.ITEM_SAPPHIRE,
-    --     ENT_TYPE.ITEM_EMERALD,
-    -- }
-    -- level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
-    --     entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
-    --     move_entity(entity.uid, 1000, 0, 0, 0)
-    --     entity:destroy()
-    -- end, SPAWN_TYPE.LEVEL_GEN, 0, removed_embedded_items)
-
     -- This is a block that will cause the player to say a message upon walking past it.
     local dialog_block_pos_x
     local dialog_block_pos_y
